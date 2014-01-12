8a43c52783b0ad5b41f619b6823ca9da232238db
Paul Smith <code@uvwxy.de>
2014-01-12 11:14:59 +0100

    misc: add cardpager layout and abstract activity to handle the layout

---
+++ src/de/uvwxy/cardpager/ActivityCardPager.java
@@ -0,0 +1,57 @@
+package de.uvwxy.cardpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import de.uvwxy.cardstyle.R;
+
+public abstract class ActivityCardPager extends FragmentActivity {
+	SectionsPagerAdapter mSectionsPagerAdapter;
+	ViewPager mViewPager;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.cardpager);
+
+
+
+
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections of the app.
+		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+		// Set up the ViewPager with the sections adapter.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+	}
+	
+	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return getFragment(position);
+		}
+
+		@Override
+		public int getCount() {
+			 return getFragmentCount();
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return getFragmentTitle(position);
+		}
+	}
+
+	public abstract Fragment getFragment(int position);
+	public abstract CharSequence getFragmentTitle(int position);
+	public abstract int getFragmentCount();
+}
